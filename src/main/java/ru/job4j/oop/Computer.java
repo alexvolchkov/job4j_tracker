7c2c5ebe3101e70f83af34994fa7193563c892a7
alexvolchkov <volkaa@mail.ru>
2021-11-27 15:22:26 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Computer.java
@@ -0,0 +1,8 @@
+package ru.job4j.oop;
+
+public class Computer {
+
+    public static void main(String[] args) {
+        Computer computer = new Computer();
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + this.active);
+        System.out.println("Статус: " + this.status);
+        System.out.println("Сообщение: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error(true, 2, "Error 1");
+        error1.printInfo();
+        System.out.println();
+        Error error2 = new Error(false, 9, "Error 2");
+        error2.printInfo();
+        System.out.println();
+        Error error3 = new Error(true, 1, "Error 3");
+        error3.printInfo();
+    }
+}
