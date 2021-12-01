88f64bb5347531261347703f04d09b1f647f13d4
alexvolchkov <volkaa@mail.ru>
2021-12-01 22:32:41 +0300

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,4 +8,47 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
 
+    @Test
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[] {"0", "Item name", "1"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item name"));
+    }
+
+    @Test
+    public void whenEditItem() {
+        Input input = new StubInput(
+                new String[] {"0", "Item one", "1", "1", "Item two", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+          new CreateAction(),
+          new EditAction(),
+          new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Item two"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Input input = new StubInput(
+                new String[] {"0", "Item one", "1", "1", "2"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new CreateAction(),
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(1), is(nullValue()));
+    }
 }
