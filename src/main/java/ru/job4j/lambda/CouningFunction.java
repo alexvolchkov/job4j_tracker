78588971cda2fd8dd940ce13172b916a630d5fa6
alexvolchkov <volkaa@mail.ru>
2021-12-11 15:58:45 +0300

    5. Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/lambda/CouningFunction.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class CouningFunction {
+
+    public static List<Double> diapason(int start, int end, Function<Double, Double> funct) {
+        List<Double> rsl = new ArrayList<>();
+        Consumer<Double> consumer = rsl :: add;
+        for (int i = start; i < end; i++) {
+            consumer.accept(funct.apply((double) i));
+        }
+        return rsl;
+    }
+}
