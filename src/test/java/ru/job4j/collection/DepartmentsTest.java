0a6e1608bfb3716ea31b079594608da20841ecc4
alexvolchkov <volkaa@mail.ru>
2021-12-21 23:19:15 +0300

    1. Метод .of [#70623 #242681]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -28,16 +28,16 @@ public class DepartmentsTest {
 
     @Test
     public void whenSortAsc() {
-        List<String> input = List.of("k1/sk1", "k2/sk1", "k1/sk2", "k2", "k2/sk2", "k1");
-        List<String> expect = List.of("k1", "k1/sk1", "k1/sk2", "k2", "k2/sk1", "k2/sk2");
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1", "k1/sk2", "k2", "k2/sk2", "k1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk2", "k2", "k2/sk1", "k2/sk2");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
     }
 
     @Test
     public void whenSortDesc() {
-        List<String> input = List.of("k1/sk1", "k2/sk1", "k1/sk2", "k2", "k2/sk2", "k1");
-        List<String> expect = List.of("k2", "k2/sk1", "k2/sk2", "k1", "k1/sk1", "k1/sk2");
+        List<String> input = Arrays.asList("k1/sk1", "k2/sk1", "k1/sk2", "k2", "k2/sk2", "k1");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k2/sk2", "k1", "k1/sk1", "k1/sk2");
         Departments.sortDesc(input);
         assertThat(input, is(expect));
     }
